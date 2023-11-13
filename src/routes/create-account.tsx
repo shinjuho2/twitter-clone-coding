07d9b17de35558cb8e34665c2185958916aae2a2
shinjuho2 <sinjuho2@gmail.com>
2023-11-13 22:05:46 +0900

    Log in

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -2,47 +2,9 @@ import { styled } from "styled-components";
 import { useState } from "react"
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%; 
-  font-size:16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { Form, NavLink, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { Error, Input, Switcher, Title, Wrapper } from "../components/auth-components";
 
 export default function CreateAccount() {
 
@@ -68,6 +30,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("")
     if(isLoading || name === "" || email === "" || password === "") return;
     try {
       setLoading(true);
@@ -78,7 +41,9 @@ export default function CreateAccount() {
       });
       navigate("/")
     } catch(e) {
-
+      if(e instanceof FirebaseError) {
+        setError(e.message)
+      }
     } finally {
       setLoading(false);
     }
@@ -116,6 +81,10 @@ export default function CreateAccount() {
         <Input type="submit" value={isLoading ? "Loading..." : "Create Account"}/>
       </Form>
       {error !== "" ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account?{" "}
+        <NavLink to="/login">Log in &rarr;</NavLink>
+      </Switcher>
     </Wrapper>
   );
 }

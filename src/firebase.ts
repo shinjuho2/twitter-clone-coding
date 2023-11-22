0c8ecdd18bb61bf66c58128f2d40044cf520b651
shinjuho2 <sinjuho2@gmail.com>
2023-11-22 22:45:47 +0900

    Tweeting

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyGSKJIyt7OUBNG2LbJm1gLghb4OptKuk",
@@ -13,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);

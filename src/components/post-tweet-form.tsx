68c2c9177f5ce59e1b48463c668b505497ae74b1
shinjuho2 <sinjuho2@gmail.com>
2023-12-06 23:00:55 +0900

    Tweeting

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
 import { styled } from "styled-components"
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
     display: flex;
@@ -69,13 +71,28 @@ export default function PostTweetForm() {
             setFile(files[0]);
         }
     }
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if( isLoading || tweet === "" || tweet.length > 180 ) return;
+        const user = auth.currentUser
+        if( !user || isLoading || tweet === "" || tweet.length > 180 ) return;
+
+        try {
+            setLoading(true);
+            await addDoc(collection(db, "tweets"), {
+                tweet,
+                createAt: Date.now(),
+                userName: user.displayName || "Anonymous",
+                userId: user.uid,
+            })
+        } catch {
+            console.log(e);
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Textarea
             rows={5}
             maxLength={180}
